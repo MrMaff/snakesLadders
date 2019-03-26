47cf55c24574b4b4975c7055aeb8a8383f242d38
MrMaff <40859552+MrMaff@users.noreply.github.com>
2019-03-26 10:50:57 +0000

    Create Program.cs
    Initial code entered

---
+++ Program.cs
@@ -0,0 +1,17 @@
+//Paste your code below
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesAndLadders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello
+        }
+    }
+}
