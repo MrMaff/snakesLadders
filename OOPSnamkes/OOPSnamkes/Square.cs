ce7114246f9fa2deb216b8bdc409ab6d24a816b3
emilygray0207 <emilygray0207@gmail.com>
2019-05-14 09:42:56 +0100

    Created Square class

---
+++ OOPSnamkes/OOPSnamkes/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSnamkes
+{
+    abstract class Square
+    {
+        private int number;
+        private char type;
+        private List<Player> occupier;
+        private int transition;
+
+        public void AddPlayer(Player player)
+        {
+
+        }
+
+        public void RemovePlayer()
+        {
+
+        }
+
+        private void SetNumber(int number)
+        {
+
+        }
+
+        public abstract void SetTransition(int num);
+
+    }
+}
