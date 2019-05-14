f4d952d8e1af57a39eeae50318009e80f3a3e42e
Alex <alexphillips@online.sch.im>
2019-05-14 09:59:38 +0100

    Adding dice and shaker classes

---
+++ OOPSnamkes/OOPSnamkes/Shaker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSnamkes
+{
+    class Shaker
+    {
+        private int totalroll;
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+
+        
+        public void GetTotal()
+        {
+
+        }
+
+        static void CompareDice()
+        {
+
+        }
+
+        static void Roll3()
+        {
+
+        }
+    }
+
+    class Dice
+    {
+        private int roll;
+
+        public int Roll
+        {
+            get
+            {
+                return roll;
+            }
+            set
+            {
+                this.roll = value;
+            }
+        }
+        public Dice()
+        {
+            Random rnd = new Random();
+            this. roll = rnd.Next(1, 7);
+        }
+
+    }
+}
