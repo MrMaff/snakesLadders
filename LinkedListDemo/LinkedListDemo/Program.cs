fd6935b6c783f50179df21427dd9736abd48b190
Mr Maff <mafflong@online.sch.im>
2020-12-14 23:28:00 +0000

    Created Example Linked List

---
+++ LinkedListDemo/LinkedListDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myLinkedList myNums = new myLinkedList();
+
+            myNums.AddFirst(6);
+            myNums.AddFirst(5);
+            myNums.AddLast(7);
+            myNums.AddLast(8);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(myNums.ElementAt(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinkedListDemo/LinkedListDemo/myLinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    class myLinkedList
+    {
+        public myListNode first;
+        public myListNode last;
+        public int count;
+
+        public void AddFirst(int _data)
+        {
+            myListNode newNode = new myListNode();
+            newNode.data = _data;
+
+            if (first == null)
+            {
+                first = newNode;
+                last = newNode;
+            }
+            else
+            {
+                newNode.nextNode = first;
+                first = newNode;
+            }
+        }
+
+        public void AddLast(int _data)
+        {
+            myListNode newNode = new myListNode();
+            newNode.data = _data;
+
+            if (first == null)
+            {
+                first = newNode;    
+            }
+            else
+            {
+                //myListNode tempNode = first;
+                //while (tempNode.nextNode != null)
+                //{
+                //    tempNode = tempNode.nextNode;
+                //}
+                //tempNode.nextNode = newNode;
+
+                last.nextNode = newNode;    
+            }
+            last = newNode;
+        }
+
+        public int ElementAt(int index)
+        {
+            myListNode tempNode = first;
+            for (int i = 0; i < index; i++)
+            {
+                tempNode = tempNode.nextNode;
+            }
+
+            return tempNode.data;
+        }
+
+    }
+
+    class myListNode
+    {
+        public int data;
+        public myListNode nextNode;
+    }
+}
