b4dab51ef025ae3f1ae5fc84d5ec57d3573490f4
SNHSMLon <SNHSMLon@IOMG21693W>
2021-01-12 13:10:10 +0000

    created hashTable structure

---
+++ demoHash/demoHash/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoHash
+{
+    struct student
+    {
+        string studentCode;
+        string surname;
+        string firstname;
+        string house;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student temp = new student();
+
+            myHashTable studentTable = new myHashTable(50);
+
+            studentTable.Remove("6656");
+            studentTable.Remove(temp);
+        }
+    }
+}
---
+++ demoHash/demoHash/myHashTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoHash
+{
+    class myHashTable
+    {
+        student[] table;
+
+        public myHashTable(int numStudents)
+        {
+            table = new student[(numStudents + 10)];
+        }
+
+        private int myHash(string key)
+        {
+            int index = 0;
+
+            //Hashing algorith
+
+            return index;
+        }
+
+        public void Add(student newRecord)
+        {
+            //extract the key
+            //hash the key
+            //put the new record into the array at the calcualted index
+            //check for collision - rehash if necessary
+        }
+
+        public void Remove(student removeData)
+        {
+            //extract the key
+            //hash the key
+            //check for collision - and rehash if necessary
+            //remove the data from the location
+
+        }
+
+        public void Remove(string studentcode)
+        {
+            //hash the key
+            //check for collision - and rehash if necessary
+            //remove the data from the location
+
+        }
+
+        public student GetStudent(string studentcode)
+        {
+            student tempStudent = new student();
+
+            //hash the key
+            //check for collision - and rehash if necessary
+
+            return tempStudent;
+        }
+
+        public bool Find(string studentCode)
+        {
+
+            return true;
+        }
+
+
+    }
+}
