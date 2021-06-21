3fe9a97dccd28d77a5b0777864f35da14b77d1c4
Mr Maff <mafflong@online.sch.im>
2021-06-21 20:44:36 +0100

    Added EnityFramework

---
+++ OOPRecords/OOPRecords.Model/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+
+namespace OOPRecords.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(string dbName) : base(dbName)
+        {
+            Database.SetIntializer(new Initializer());
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
